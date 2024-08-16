0a3df7909af9a16c1c6a2ec3894f4c337aa42deb
Tolmachev Alexandr <167399077+ggezz007@users.noreply.github.com>
2024-08-16 17:27:54 +0200

    new fucntions + new structure

--- main.cpp
+++ main.cpp
@@ -3,36 +3,39 @@
 #include <math.h>
 #define EPS 1e-9
 #define INF -1
-typedef struct solvation solvation;
+typedef struct solution solution;
+typedef struct data data;
 
-struct solve{
+struct solution{
     int cnt;
     double x1, x2;
 };
-
-void input(double *, double *, double *);
-void output(solve);
+struct data{
+    double a, b, c;
+};
+void input(data *);
+void output(const solution);
 double dsmt(const double,const double, const double );
-solve solutions(const double, const double, const double);
+solution solve(const data);
 
 int main() {
-    double a, b, c;
-    input(&a, &b, &c);
-    solve ans = solutions(a, b, c);
+    data koef;
+    input(&koef);
+    solution ans = solve(koef);
     output(ans);
 }
 
-void input( double * a,  double * b,  double * c) {
+void input(data * koef) {
     printf("your coefficients for quadratic equation (a * x^2 + b * x + c = 0): ");
     printf("\nalso remember that b^2, 4*a*c, b^2+4*a*c should be the size  double, so smaller than 1.7E+308");
     printf("\na =");
-    scanf(" %lf", a);
+    scanf(" %lf", &(koef->a));
     printf("b =");
-    scanf(" %lf", b);
+    scanf(" %lf", &(koef->b));
     printf("c =");
-    scanf(" %lf", c);
+    scanf(" %lf",  &(koef->c));
 }
-void output(solve ans) {
+void output(const solution ans) {
     if (ans.cnt == INF) {
         printf("your equation has INF solutions in R quantity");
     } else if (ans.cnt == 0) {
@@ -49,31 +52,59 @@ void output(solve ans) {
 double dsmt(const double a,const double  b,const double  c) {
     return b*b-4*a*c;
 }
-
-solve solutions(const double a,const double b,const double c) {
-    double x1, x2;
-    if (abs(a) < EPS) {
-        if (abs(b) < EPS) {
-            if (abs(c) < EPS) {
-                return (solve) {INF, -1, -1};
-            } else {
-                return (solve) {0, -1, -1};
+solution inf_eq(const data koef) {
+    if (abs(koef.a) < EPS) {
+        if (abs(koef.b) < EPS) {
+            if (abs(koef.c) < EPS) {
+                return (solution) {INF, -1, -1};
             }
-        } else {
-            x1 = -c/b;
-            return (solve) {1, x1, -1};
         }
     }
-    double d = dsmt(a, b, c);
+    return (solution) {-2, -2, -2};
+}
+solution zero_eq(const data koef) {
+    if (abs(koef.a) < EPS) {
+        if (abs(koef.b) < EPS) {
+            return (solution) {0, -1, -1};
+        }
+    }
+    return (solution) {-2, -2, -2};
+}
+solution line_eq(const data koef) {
+    if (abs(koef.a) < EPS) {
+        return (solution) {1, -koef.c/koef.b, -1};
+    }
+    return (solution) {-2, -2, -2};
+}
+solution square_eq(const data koef) {
+    double d = dsmt(koef.a, koef.b, koef.c), x1, x2;
     if (d < 0.0 && abs(d) > EPS) {
-        return (solve) {0, -1, -1};
+        return (solution) {0, -1, -1};
     }
     if (abs(d) < EPS) {
-        x1 = -(b)/(2*(a));
-        return (solve) {1, x1, -1};
+        x1 = -(koef.b)/(2*(koef.a));
+        return (solution){1, x1, -1};
     }
-    x1 = (-b + sqrt(d))/(2*a);
-    x2 = (-b - sqrt(d))/(2*a);
-    return (solve) {2, x1, x2};
+    x1 = (-koef.b + sqrt(d))/(2*koef.a);
+    x2 = (-koef.b - sqrt(d))/(2*koef.a);
+    return (solution) {2, x1, x2};
 }
+solution solve(const data koef) {
+    solution ans;
+    ans = inf_eq(koef);
+    if (ans.cnt != -2) {
+        return ans;
+    }
+    ans = zero_eq(koef);
+    if (ans.cnt != -2) {
+        return ans;
+    }
+    ans = line_eq(koef);
+    if (ans.cnt != -2) {
+        return ans;
+    }
+    ans = square_eq(koef);
+    return ans;
+}
+
 
