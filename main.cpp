2c6ea4b65b80b4e1712d06c21d7347dc297cd4ac
Tolmachev Alexandr <167399077+ggezz007@users.noreply.github.com>
2024-08-16 16:08:09 +0200

    renamed new struct + new solutions

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@
 #define INF -1
 typedef struct solvation solvation;
 
-struct solvation{
+struct solve{
     int cnt;
     double x1, x2;
 };
 
 void input(double *, double *, double *);
-void output(solvation);
+void output(solve);
 double dsmt(const double,const double, const double );
-solvation solutions(const double, const double, const double);
+solve solutions(const double, const double, const double);
 
 int main() {
     double a, b, c;
     input(&a, &b, &c);
-    solvation ans = solutions(a, b, c);
+    solve ans = solutions(a, b, c);
     output(ans);
 }
 
@@ -32,7 +32,7 @@ void input( double * a,  double * b,  double * c) {
     printf("c =");
     scanf(" %lf", c);
 }
-void output(solvation ans) {
+void output(solve ans) {
     if (ans.cnt == INF) {
         printf("your equation has INF solutions in R quantity");
     } else if (ans.cnt == 0) {
@@ -50,30 +50,30 @@ double dsmt(const double a,const double  b,const double  c) {
     return b*b-4*a*c;
 }
 
-solvation solutions(const double a,const double b,const double c) {
+solve solutions(const double a,const double b,const double c) {
     double x1, x2;
     if (abs(a) < EPS) {
         if (abs(b) < EPS) {
             if (abs(c) < EPS) {
-                return (solvation) {INF, -1, -1};
+                return (solve) {INF, -1, -1};
             } else {
-                return (solvation) {0, -1, -1};
+                return (solve) {0, -1, -1};
             }
         } else {
             x1 = -c/b;
-            return (solvation) {1, x1, -1};
+            return (solve) {1, x1, -1};
         }
     }
     double d = dsmt(a, b, c);
     if (d < 0.0 && abs(d) > EPS) {
-        return (solvation) {0, -1, -1};
+        return (solve) {0, -1, -1};
     }
     if (abs(d) < EPS) {
         x1 = -(b)/(2*(a));
-        return (solvation) {1, x1, -1};
+        return (solve) {1, x1, -1};
     }
     x1 = (-b + sqrt(d))/(2*a);
     x2 = (-b - sqrt(d))/(2*a);
-    return (solvation) {2, x1, x2};
+    return (solve) {2, x1, x2};
 }
 
