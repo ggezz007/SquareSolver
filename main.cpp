80aab4597ffd94c112005a79a6404577ae9fa068
Tolmachev Alexandr <167399077+ggezz007@users.noreply.github.com>
2024-08-17 01:21:23 +0200

    fixed solve + beauty

--- main.cpp
+++ main.cpp
@@ -1,40 +1,51 @@
 #include <TXLib.h>
 #include <stdio.h>
 #include <math.h>
+
 #define EPS 1e-9
 #define INF -1
-typedef struct solution solution;
-typedef struct data data;
-
-struct solution{
+struct solution {
     int cnt;
     double x1, x2;
 };
-struct data{
+struct data {
     double a, b, c;
 };
-void input(data *);
-void output(const solution);
-double dsmt(const double,const double, const double );
-solution solve(const data);
+
+void input(data *coeff);
+
+void output(const solution ans);
+
+double get_diskriminant(const double a, const double b, const double c);
+
+solution inf_eq(const data coeff);
+
+solution zero_eq(const data coeff);
+
+solution line_eq(const data coeff);
+
+solution square_eq(const data coeff);
+
+solution solve(const data coeff);
 
 int main() {
-    data koef;
-    input(&koef);
-    solution ans = solve(koef);
+    data coeff;
+    input(&coeff);
+    solution ans = solve(coeff);
     output(ans);
 }
 
-void input(data * koef) {
+void input(data *coeff) {
     printf("your coefficients for quadratic equation (a * x^2 + b * x + c = 0): ");
     printf("\nalso remember that b^2, 4*a*c, b^2+4*a*c should be the size  double, so smaller than 1.7E+308");
     printf("\na =");
-    scanf(" %lf", &(koef->a));
+    scanf(" %lf", &(coeff->a));
     printf("b =");
-    scanf(" %lf", &(koef->b));
+    scanf(" %lf", &(coeff->b));
     printf("c =");
-    scanf(" %lf",  &(koef->c));
+    scanf(" %lf", &(coeff->c));
 }
+
 void output(const solution ans) {
     if (ans.cnt == INF) {
         printf("your equation has INF solutions in R quantity");
@@ -49,62 +60,48 @@ void output(const solution ans) {
         printf("\nx2 = %lf", ans.x2);
     }
 }
-double dsmt(const double a,const double  b,const double  c) {
-    return b*b-4*a*c;
+
+double get_diskriminant(const double a, const double b, const double c) {
+    return b * b - 4 * a * c;
 }
-solution inf_eq(const data koef) {
-    if (abs(koef.a) < EPS) {
-        if (abs(koef.b) < EPS) {
-            if (abs(koef.c) < EPS) {
-                return (solution) {INF, -1, -1};
-            }
-        }
-    }
-    return (solution) {-2, -2, -2};
+
+solution inf_eq(const data coeff) {
+    return (solution) {INF, -1, -1};
 }
-solution zero_eq(const data koef) {
-    if (abs(koef.a) < EPS) {
-        if (abs(koef.b) < EPS) {
-            return (solution) {0, -1, -1};
-        }
-    }
-    return (solution) {-2, -2, -2};
+
+solution zero_eq(const data coeff) {
+    return (solution) {0, -1, -1};
 }
-solution line_eq(const data koef) {
-    if (abs(koef.a) < EPS) {
-        return (solution) {1, -koef.c/koef.b, -1};
-    }
-    return (solution) {-2, -2, -2};
+
+solution line_eq(const data coeff) {
+    return (solution) {1, -coeff.c / coeff.b, -1};
 }
-solution square_eq(const data koef) {
-    double d = dsmt(koef.a, koef.b, koef.c), x1, x2;
+
+solution square_eq(const data coeff) {
+    double d = get_diskriminant(coeff.a, coeff.b, coeff.c), x1, x2;
     if (d < 0.0 && abs(d) > EPS) {
         return (solution) {0, -1, -1};
     }
     if (abs(d) < EPS) {
-        x1 = -(koef.b)/(2*(koef.a));
-        return (solution){1, x1, -1};
+        x1 = -(coeff.b) / (2 * (coeff.a));
+        return (solution) {1, x1, -1};
     }
-    x1 = (-koef.b + sqrt(d))/(2*koef.a);
-    x2 = (-koef.b - sqrt(d))/(2*koef.a);
+    x1 = (-coeff.b + sqrt(d)) / (2 * coeff.a);
+    x2 = (-coeff.b - sqrt(d)) / (2 * coeff.a);
     return (solution) {2, x1, x2};
 }
-solution solve(const data koef) {
-    solution ans;
-    ans = inf_eq(koef);
-    if (ans.cnt != -2) {
-        return ans;
+
+solution solve(const data coeff) {
+    if (abs(coeff.a) < EPS) {
+        if (abs(coeff.b) < EPS) {
+            if (abs(coeff.c) < EPS) {
+                return inf_eq(coeff);
+            } else {
+                return zero_eq(coeff);
+            }
+        } else {
+            return line_eq(coeff);
+        }
     }
-    ans = zero_eq(koef);
-    if (ans.cnt != -2) {
-        return ans;
-    }
-    ans = line_eq(koef);
-    if (ans.cnt != -2) {
-        return ans;
-    }
-    ans = square_eq(koef);
-    return ans;
+    return square_eq(coeff);
 }
-
-
