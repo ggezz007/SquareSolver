b887da4cf21f09a7d4f46238a1db99a22acfb32b
Tolmachev Alexandr <167399077+ggezz007@users.noreply.github.com>
2024-08-20 21:10:55 +0200

    added switch+case, fflush, is_zero

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <TXLib.h>
+//#include <TXLib.h>
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 #define EPS 1e-9
-#define maxn 100
 
 enum RootsNumber {
     CNT_INFINITY = -1,
@@ -17,7 +17,7 @@ enum Status {
     CORRECT = 1
 };
 
-struct solution {
+struct Solution {
     RootsNumber cnt;
     double x1;
     double x2;
@@ -29,145 +29,134 @@ struct data {
     double c;
 };
 
-Status input(data *coeff);
+void input(data *coeff);
 
 Status input_coef_x(double *d);
 
-void output(const solution ans);
+void output(const Solution ans);
 
 void output_wrong();
 
 double get_diskriminant(const double a, const double b, const double c);
 
-solution line_eq(const data coeff);
+Solution line_eq(const data coeff);
 
-solution square_eq(const data coeff);
+Solution square_eq(const data coeff);
 
-solution solve(const data coeff);
+Solution solve(const data coeff);
 
-int compare(const double d1, const double d2);
+int is_zero(const double d);
 
 int main() {
     data coeff = {};
-    int correct_input_check = input(&coeff);
-    if (!correct_input_check) {
-        output_wrong();
-        return 0;
-    }
-    solution ans = solve(coeff);
+    input(&coeff);
+    Solution ans = solve(coeff);
     output(ans);
     return 0;
 }
 
 Status input_coef_x(double *d) {
-    char STR[maxn];
-    fgets(STR, maxn, stdin);
-    int point_check = 0;
-    int minus_check = 0;
-    for (int i = 0; i < maxn; i++) {
-        if (STR[i] == '\0' || STR[i] == '\n') {
-            break;
-        }
-        if (STR[i] == '.' && point_check == 0) {
-            point_check++;
-            continue;
-        }
-        if (STR[i] == '.' && point_check == 1) {
-            return WRONG;
-        }
-        if (STR[i] == '-' && minus_check == 0 && i == 0) {
-            minus_check++;
-            continue;
-        }
-        if (STR[i] == '-' && minus_check == 1) {
-            return WRONG;
-        }
-        if (STR[i] - '0' <= 9 && STR[i] - '0' >= 0) {
-            continue;
-        }
+    assert(d != NULL);
+    int first_checker = scanf("%lf", d);
+    if (!first_checker) {
+        fflush(stdin);
         return WRONG;
     }
-    *d = atof(STR);
+    char symbol;
+    while ((symbol = (char)getchar()) != EOF) {
+        if (symbol=='\n') {
+            fflush(stdin);
+            return CORRECT;
+        } else {
+            fflush(stdin);
+            return WRONG;
+        }
+    }
     return CORRECT;
-
 }
 
-Status input(data *coeff) {
+void input(data *coeff) {
+    assert(coeff != NULL);
     printf("your coefficients for quadratic equation (a * x ^ 2 + b * x + c = 0): \n"
            "also remember that b ^ 2, 4 * a * c, b ^ 2 + 4 * a * c should be the size double, so smaller than 1.7E+308\n"
            "a = ");
-    if (!input_coef_x(&(coeff->a))) {
-        return WRONG;
+    while (!input_coef_x(&(coeff->a))) {
+        output_wrong();
     }
     printf("b = ");
-    if (!input_coef_x(&(coeff->b))) {
-        return WRONG;
+    while (!input_coef_x(&(coeff->b))) {
+        output_wrong();
     }
     printf("c = ");
-    if (!input_coef_x(&(coeff->c))) {
-        return WRONG;
+    while (!input_coef_x(&(coeff->c))) {
+        output_wrong();
     }
-    return CORRECT;
 }
 
-void output(const solution ans) {
-    if (ans.cnt == CNT_INFINITY) {
-        printf("your equation has INF solutions in R quantity\n");
-    } else if (ans.cnt == CNT_ZERO) {
-        printf("your equation has not solutions in R quantity\n");
-    } else if (ans.cnt == CNT_ONE) {
-        printf("your equation has one solution in R quantity:\n"
-               "x = %lf\n", ans.x1);
-    } else {
-        printf("your equation has two solutions in R quantity:\n"
-               "x1 = %lf\n"
-               "x2 = %lf\n", ans.x1, ans.x2);
+void output(const Solution ans) {
+    switch (ans.cnt) {
+        case CNT_INFINITY:
+            printf("your equation has INF Solutions in R quantity\n");
+            break;
+        case CNT_ZERO:
+            printf("your equation has not Solutions in R quantity\n");
+            break;
+        case CNT_ONE:
+            printf("your equation has one Solution in R quantity:\n"
+                   "x = %lf\n", ans.x1);
+            break;
+        case CNT_TWO:
+            printf("your equation has two Solutions in R quantity:\n"
+                   "x1 = %lf\n"
+                   "x2 = %lf\n", ans.x1, ans.x2);
+            break;
+        default: assert(0 && "ans.cnt is incorrect");
     }
 }
 
 void output_wrong() {
-    printf("input should contain only numbers from range 0-9, '.', '-'");
+    printf("input should contain only numbers from range 0-9, '.', '-'.\n"
+           "Please try again\n");
+}
+
+int is_zero(const double d) {
+    return d < EPS;
 }
 
 double get_diskriminant(const double a, const double b, const double c) {
     return b * b - 4 * a * c;
 }
 
-int compare(const double d1, const double d2) {
-    return d1 < d2;
-}
-
-solution line_eq(const data coeff) {
-    if (compare(fabs(coeff.b), EPS)) {
-        if (compare(fabs(coeff.c), EPS)) {
-            return (solution) {CNT_INFINITY, -1, -1};
+Solution line_eq(const data coeff) {
+    if (is_zero(fabs(coeff.b))) {
+        if (is_zero(fabs(coeff.c))) {
+            return (Solution) {CNT_INFINITY, -1, -1};
         } else {
-            return (solution) {CNT_ZERO, -1, -1};
+            return (Solution) {CNT_ZERO, -1, -1};
         }
     }
-    return (solution) {CNT_ONE, -coeff.c / coeff.b, -1};
+    return (Solution) {CNT_ONE, -coeff.c / coeff.b, -1};
 }
 
-solution square_eq(const data coeff) {
+Solution square_eq(const data coeff) {
     double d = get_diskriminant(coeff.a, coeff.b, coeff.c);
     double x1 = -1;
     double x2 = -1;
-    if (compare(d, 0.0) && !compare(fabs(d), EPS)) {
-        return (solution) {CNT_ZERO, -1, -1};
+    if (d < 0.0 && !is_zero(fabs(d))) {
+        return (Solution) {CNT_ZERO, -1, -1};
     }
-    if (compare(fabs(d), EPS)) {
+    if (is_zero(fabs(d))) {
         x1 = -coeff.b / (2 * coeff.a);
-        return (solution) {CNT_ONE, x1, -1};
+        return (Solution) {CNT_ONE, x1, -1};
     }
     x1 = (-coeff.b + sqrt(d)) / (2 * coeff.a);
     x2 = (-coeff.b - sqrt(d)) / (2 * coeff.a);
-    return (solution) {CNT_TWO, x1, x2};
+    return (Solution) {CNT_TWO, x1, x2};
 }
 
-solution solve(const data coeff) {
-    if (compare(fabs(coeff.a), EPS)) {
+Solution solve(const data coeff) {
+    if (is_zero(fabs(coeff.a))) {
         return line_eq(coeff);
     }
     return square_eq(coeff);
 }
-
